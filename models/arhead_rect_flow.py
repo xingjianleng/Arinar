4cc20dac3bc723e985f9c71638740a0c3ab4e561
Qinyu Zhao <qinyu.zhao@anu.edu.au>
2025-04-08 07:31:55 +1000

    Rect flow cfg

--- models/arhead_rect_flow.py
+++ models/arhead_rect_flow.py
@@ -97,20 +97,25 @@ class ARHead_rect_flow(nn.Module):
                 x = b(x=x, cond_BD=z, attn_bias=None, causal=False)
             x = x.squeeze(1)
 
-            x_next = torch.randn(bsz, 1, device=z.device)
+            if cfg == 1.0:
+                x_next = torch.randn(bsz, 1, device=z.device)
+            else:
+                x_next = torch.randn(bsz // 2, 1, device=z.device)
             t_steps = torch.linspace(0, 1, self.num_sampling_steps+1, dtype=torch.float32)
 
             for i, (t_cur, t_next) in enumerate(zip(t_steps[:-1], t_steps[1:])):
-                x_cur = x_next
+                x_cur = x_next if cfg == 1.0 else torch.cat([x_next, x_next], dim=0)
                 time_input = torch.ones(x_cur.size(0), device=z.device, dtype=torch.float32) * t_cur
                 d_cur = self.net(x_cur, time_input, x)
                 if not cfg == 1.0:
                     d_cur_cond, d_cur_uncond = d_cur.chunk(2)
                     d_cur = d_cur_uncond + cfg * (d_cur_cond - d_cur_uncond)
-                x_next = x_cur + (t_next - t_cur) * d_cur
+                x_next = x_cur[:x_next.shape[0]] + (t_next - t_cur) * d_cur
 
-            res.append(x_next)
+            if not cfg == 1.0:
+                x_next = torch.cat([x_next, x_next], dim=0)
 
+            res.append(x_next)            
             x = self.input_proj(x_next.unsqueeze(-1))
         
         for b in self.blocks: b.attn.kv_caching(False)
