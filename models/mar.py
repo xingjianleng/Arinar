b260d4e90707c9971389df5b049784c7ddd8c1e0
Qinyu Zhao <qinyu.zhao@anu.edu.au>
2025-05-03 15:58:54 +1000

    DDIM sampling; Probing MAR

--- models/mar.py
+++ models/mar.py
@@ -415,8 +415,11 @@ def mar_large(**kwargs):
 
 
 def mar_huge(**kwargs):
+    encoder_depth = decoder_depth = kwargs.pop('enc_dec_depth')
+    if encoder_depth < 0:
+        encoder_depth = decoder_depth = 22
     model = MAR(
-        encoder_embed_dim=1280, encoder_depth=22, encoder_num_heads=16,
-        decoder_embed_dim=1280, decoder_depth=22, decoder_num_heads=16,
+        encoder_embed_dim=1280, encoder_depth=encoder_depth, encoder_num_heads=16,
+        decoder_embed_dim=1280, decoder_depth=decoder_depth, decoder_num_heads=16,
         mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     return model
